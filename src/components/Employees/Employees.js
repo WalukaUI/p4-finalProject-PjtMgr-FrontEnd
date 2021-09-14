92f49b06aba6d8d073fae385f9077e4d3e1dc07c
WalukaUI <cwaluka@yahoo.com>
2021-09-14 00:47:13 -0700

    cities added

---
+++ src/components/Employees/Employees.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import "./Employees.css";
+
+function Employees() {
+  const [employees, setEmployees] = useState([]);
+
+  const URL = "https://project-manager-bkend.herokuapp.com";
+
+  useEffect(() => {
+    fetch(`${URL}/employees`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((r) => r.json())
+      .then((data) => setEmployees(data));
+  }, []);
+
+  return (<>
+        {employees.map((c, idx) => {
+          return (<div class="card text-white bg-secondary mb-3 employeeCard" key={c.name + idx}>
+            <div className="card-header">{c.name}</div>
+            <div className="card-body">
+              <h6 className="card-title">Branch Name: {c.branch_name}</h6>
+            </div>
+          </div>
+          )
+        })}
+    </>
+  );
+}
+
+export default Employees;
