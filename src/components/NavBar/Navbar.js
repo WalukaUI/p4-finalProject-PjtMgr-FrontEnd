e14c3102cf3a5c0c85caf7eac7e94a0bb3a904b8
WalukaUI <cwaluka@yahoo.com>
2021-09-13 17:15:40 -0700

    nav bar edited

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,6 +1,6 @@
 import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 import React from "react";
-// import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 import "./Navbar.css";
 
 function Nav2() {
@@ -22,7 +22,7 @@ function Nav2() {
                   alt="SLA Logo"
                   className="logoImageNav2"
                 />
-                T&T.Inc Project Mgt Sys
+                T&T.Inc Project Mgt. Sys
               </Navbar.Brand>
             </div>
 
@@ -30,26 +30,24 @@ function Nav2() {
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
               <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link href="/about">About Us</Nav.Link>
-                  <Nav.Link href="/events">Events</Nav.Link>
-
-                  <NavDropdown
-                    title="More"
-                    id="collasible-nav-dropdown"
-                  >
-                    <NavDropdown.Item href="/gallery">
-                      Photo Gallery
+                  <Nav.Link href="/projects">Projects</Nav.Link>
+                  <Nav.Link href="/countries">Countries</Nav.Link>
+                  <Nav.Link href="/cities">Cities</Nav.Link>
+                  <NavDropdown title="More" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="/departments">
+                      Departments
                     </NavDropdown.Item>
-                    <NavDropdown.Item href="/members">
-                      committee Members
+                    <NavDropdown.Item href="/employees">
+                      Employees
                     </NavDropdown.Item>
-                    <NavDropdown.Item href="/history">History</NavDropdown.Item>
-                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="/history"></NavDropdown.Item>
                   </NavDropdown>
-                  <Nav.Link href="/contact">Contact Us</Nav.Link>
                 </Nav>
                 <Nav.Link href="/create">
-                  <button className="btn btn-warning">Become a Member</button>
+                  <button className="btn btn-warning">Log in</button>
+                </Nav.Link>
+                <Nav.Link href="/create">
+                  <button className="btn btn-warning">Sign up</button>
                 </Nav.Link>
               </Navbar.Collapse>
             </div>
