2395558bfcd5bbea4604667124ca1c1cac6fa70b
WalukaUI <cwaluka@yahoo.com>
2021-09-14 02:48:03 -0700

    Home page created

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,33 @@ import "./Home.css";
 function Home() {
 
   return (<>
-     <img src="./img/mgt2.jpg" alt="mgt" className="homeImage"/>
+  <div className="homeDiv">
+    <div className="homeCards">
+       <a href="/countries">Countries
+       <img src="./img/homeimg/city.jpg" alt="mgt" className="homeImage"/></a>
+    </div>
+    <div className="homeCards">
+      <a href="/employees" >Employees
+       <img src="./img/homeimg/employee.jpg" alt="mgt" className="homeImage"/></a>
+    </div>
+    <div className="homeCards">
+      <a href="/departments">Departments
+       <img src="./img/homeimg/dpt.jpg" alt="mgt" className="homeImage"/></a>
+    </div>
+    <div className="homeCards">
+      <a href="/projects">Projects
+       <img src="./img/homeimg/project.jpg" alt="mgt" className="homeImage"/></a>
+    </div>
+    <div className="homeCards">
+      <a href="/countries">Countries
+       <img src="./img/homeimg/country.jpg" alt="mgt" className="homeImage"/></a>
+    </div>
+    <div className="homeCards">
+      <a href="/cities">Cities
+       <img src="./img/homeimg/extra.jpg" alt="mgt" className="homeImage"/></a>
+    </div>
+  </div>
+     
     </>
   );
 }
