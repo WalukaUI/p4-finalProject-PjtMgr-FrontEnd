cf3147a78646f5f7211e91952b8fa936e0b50212
WalukaUI <cwaluka@yahoo.com>
2023-06-24 10:13:21 -0500

    constrains file links changed

--- src/constraints/URL.js
+++ src/constraints/URL.js
@@ -2,6 +2,9 @@
 const BASE_URL=
 process.env.NODE_ENV === "development"
 ?"http://localhost:3000"
-:"https://project-manager-bkend.herokuapp.com"
+:"https://tnt-project-management-app.onrender.com"
 
 export default BASE_URL
+
+// render backend= https://tnt-project-management-app.onrender.com
+// heroku backend = https://project-manager-bkend.herokuapp.com
