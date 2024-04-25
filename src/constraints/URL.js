37528c2946285e897b2d0d3b49afc8794d85ec86
WalukaUI <cwaluka@yahoo.com>
2024-04-25 15:27:48 -0500

    added new bkend url

--- src/constraints/URL.js
+++ src/constraints/URL.js
@@ -2,9 +2,10 @@
 const BASE_URL=
 process.env.NODE_ENV === "development"
 ?"http://localhost:3000"
-:"https://cnc-and-docapp-webservice.onrender.com"
+:"https://twodbsin1bkend.onrender.com"
 
 export default BASE_URL
 
 // render backend= https://tnt-project-management-app.onrender.com
 // heroku backend = https://project-manager-bkend.herokuapp.com
+//https://cnc-and-docapp-webservice.onrender.com
